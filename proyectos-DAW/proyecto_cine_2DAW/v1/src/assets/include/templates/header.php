7db00b11e9017958ae67081a36e5f5bd26288d7e
hugossanchezz <hugosanchezciudad@gmail.com>
2025-01-22 19:58:13 +0100

    v1-proyecto

---
+++ proyectos-DAW/proyecto_cine_2DAW/v1/src/assets/include/templates/header.php
@@ -0,0 +1,45 @@
+<header class="flex">
+    <section class="section-logo">
+        <a href="index.html">
+            <img class="logo" src="../src/assets/img/logo-no-cd.png" alt="Logo" />
+        </a>
+    </section>
+
+    <nav class="flex">
+        <div class="nav__item">
+            <a class="font-size-pequenio" href="nosotros.html">Nosotros</a>
+        </div>
+        <div class="nav__item">
+            <a class="font-size-pequenio" href="estrenos.html">Estrenos</a>
+        </div>
+        <div class="nav__item">
+            <a class="font-size-pequenio" href="peliculas.html">Películas</a>
+        </div>
+        <div class="nav__item">
+            <a class="font-size-pequenio" href="series.html">Series</a>
+        </div>
+        <div class="nav__item">
+            <a class="font-size-pequenio flex" href="mi-espacio.html">
+                Mi espacio
+                <img
+                    src="../src/assets/img/ico/bookmark.svg"
+                    alt="Icono de guardado"
+                    srcset="" />
+            </a>
+        </div>
+    </nav>
+    <section class="acciones flex">
+        <div class="acciones__busqueda">
+            <img
+                class="busqueda__icono"
+                src="../src/assets/img/ico/lupa.svg"
+                alt="Icono de búsqueda" />
+        </div>
+        <div class="acciones__login">
+            <img
+                class="login__icono"
+                src="../src/assets/img/ico/login.svg"
+                alt="Icono de inicio de sesión" />
+        </div>
+    </section>
+</header>
