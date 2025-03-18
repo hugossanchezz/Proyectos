7197199ab77d89d6f6373c4a8ae3f17ec65b1539
_hugossanchezz_ <hugosanchezciudad23@gmail.com>
2025-03-18 11:39:43 +0100

    lv-feat: creación de migrations

---
+++ AprendiendoLaravelVue/LaravelVue/database/migrations/2025_03_17_170733_alter_books_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->unsignedBigInteger('author_id')->nullable();
+            
+            // la columna author_id se relaciona con la columna id de la tabla authors y si se borra el autor se pone en null, si fuera "cascade" se borran sus libros
+            $table->foreign('author_id')->references('id')->on('authors')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
