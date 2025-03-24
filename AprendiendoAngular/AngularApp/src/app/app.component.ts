4ce813a4ef246fc54c1fde10ea5686f9c2ff7fba
_hugossanchezz_ <hugosanchezciudad23@gmail.com>
2025-03-24 15:18:50 +0100

    ang-feat: app

--- AprendiendoAngular/AngularApp/src/app/app.component.ts
+++ AprendiendoAngular/AngularApp/src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { BotonEnlace } from './interfaces/boton-enlace.interface';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'AngularApp';
+
+  public botonesToolbar: BotonEnlace[]=[
+    {
+      label: 'lista',
+      ruta: ['/', 'lista'],
+    },
+    {
+      label:'formulario',
+      ruta: ['/','formulario']
+    }
+  ]
 }
--- AprendiendoAngular/AngularApp/src/app/app.routes.ts
+++ AprendiendoAngular/AngularApp/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'lista',
+    loadComponent: () => import('./pages/lista/lista.component').then(m => m.ListaComponent)
+  },
+  {
+    path: 'formulario',
+    loadComponent: () => import('./pages/formulario/formulario.component').then(m => m.FormularioComponent)
+  },
+  {
+    path: '404',
+    loadComponent: () => import('./pages/not-found/not-found.component').then(m => m.NotFoundComponent)
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '404'
+  }
+];
