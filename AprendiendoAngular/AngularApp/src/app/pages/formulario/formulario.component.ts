1c4346f774deeea8daed8c4b5c6a524aa3ef85d2
_hugossanchezz_ <hugosanchezciudad23@gmail.com>
2025-03-25 15:04:09 +0100

    ang-feat: pipe y filtrado en el formulario

--- AprendiendoAngular/AngularApp/src/app/pages/formulario/formulario.component.ts
+++ AprendiendoAngular/AngularApp/src/app/pages/formulario/formulario.component.ts
@@ -1,6 +1,8 @@
-import { Component,inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Usuario } from '../../interfaces/usuario.interface';
 import { UsuariosService } from '../../services/usuarios.service';
+import { Subject, tap } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import {
   FormControl,
@@ -16,21 +18,38 @@ import {
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.scss',
 })
-export class FormularioComponent {
+export class FormularioComponent implements OnInit {
   public servicioUsuario = inject(UsuariosService);
 
+  public destroyed$ = new Subject();
+
   public formulario: FormGroup = new FormGroup({
-    nombre: new FormControl('',[Validators.required]),
-    apellido: new FormControl('',[Validators.required]),
-    email: new FormControl('',[Validators.email]),
+    nombre: new FormControl('', [Validators.required]),
+    apellido: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.email, Validators.required]),
     telefono: new FormControl(''),
-    politicas: new FormControl('',[Validators.requiredTrue])
+    politicas: new FormControl('', [Validators.requiredTrue]),
   });
+
+  ngOnInit(): void {
+    this.servicioUsuario
+      .getUsuarios$()
+      .pipe(
+        takeUntil(this.destroyed$),
+        tap((usuarios) => {})
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
+  }
 
   public onSubmit(): void {
     const valores = this.formulario.value;
 
-    const nuevoUsuario: Usuario = {
+    const nuevoUsuario: Partial<Usuario> = {
       nombre: valores.nombre,
       apellido: valores.apellido,
       email: valores.email || null,
@@ -39,10 +58,11 @@ export class FormularioComponent {
 
     console.log(nuevoUsuario);
 
-    this.servicioUsuario.usuarios.push(nuevoUsuario);
+    this.servicioUsuario.addUsuario(nuevoUsuario);
+
+    alert(this.servicioUsuario.addUsuario(nuevoUsuario));
 
     this.limpiarFormulario();
-
   }
 
   public limpiarFormulario(): void {
@@ -51,7 +71,7 @@ export class FormularioComponent {
       apellido: '',
       email: '',
       telefono: '',
-      politicas: false
+      politicas: false,
     });
   }
 }
