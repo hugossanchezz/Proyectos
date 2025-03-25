fd133f52ab75662523fc6d4a8875b09784a7216f
_hugossanchezz_ <hugosanchezciudad23@gmail.com>
2025-03-25 15:03:37 +0100

    ang-fix: mejorar diseño y funcionalidad de listar usuarios

--- AprendiendoAngular/AngularApp/src/app/pages/lista/lista.component.ts
+++ AprendiendoAngular/AngularApp/src/app/pages/lista/lista.component.ts
@@ -1,14 +1,44 @@
 import { Component, inject } from '@angular/core';
 import { UsuariosService } from '../../services/usuarios.service';
+import { TarjetaComponent } from '../../components/tarjeta/tarjeta.component';
+import { BotonEnlaceComponent } from '../../components/boton-enlace/boton-enlace.component';
+import { Usuario } from '../../interfaces/usuario.interface';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, Observable, tap } from 'rxjs';
+import { AsyncPipe, UpperCasePipe } from '@angular/common';
+import { FiltrarNombrePipe } from '../../pipes/filtrar-nombre.pipe';
 
 @Component({
   selector: 'app-lista',
-  imports: [],
+  imports: [
+    TarjetaComponent,
+    BotonEnlaceComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    UpperCasePipe,
+    FiltrarNombrePipe,
+    AsyncPipe,
+  ],
   templateUrl: './lista.component.html',
-  styleUrl: './lista.component.scss'
+  styleUrl: './lista.component.scss',
 })
 export class ListaComponent {
   public servicioUsuario = inject(UsuariosService);
+
+  public usuarios$: Observable<Usuario[] | undefined> =
+    this.servicioUsuario.getUsuarios$();
+
+  public filtroBusqueda: FormControl = new FormControl('');
+  public cadenaBusqueda: string = '';
+
+  ngOnInit(): void {
+    this.filtroBusqueda.valueChanges
+      .pipe(
+        debounceTime(500),
+        tap((valor) => {
+          this.cadenaBusqueda = valor || '';
+        })
+      )
+      .subscribe();
+  }
 }
-
-
