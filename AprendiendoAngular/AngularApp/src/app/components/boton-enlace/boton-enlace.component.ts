e834dc23c0bc2ae6ef43adaf476eb1091aeffb7f
_hugossanchezz_ <hugosanchezciudad23@gmail.com>
2025-03-25 14:58:28 +0100

    ang-fix: atributo color en componente boton

--- AprendiendoAngular/AngularApp/src/app/components/boton-enlace/boton-enlace.component.ts
+++ AprendiendoAngular/AngularApp/src/app/components/boton-enlace/boton-enlace.component.ts
@@ -1,13 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { BotonEnlace } from '../../interfaces/boton-enlace.interface';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-boton-enlace',
-  imports: [RouterLink],
+  imports: [RouterLink,NgClass],
   templateUrl: './boton-enlace.component.html',
   styleUrl: './boton-enlace.component.scss',
 })
 export class BotonEnlaceComponent {
   @Input() public boton?: BotonEnlace;
+
+  @Input() public color?: string;
 }
